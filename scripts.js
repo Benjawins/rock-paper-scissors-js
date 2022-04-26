95c0ea9be6eae57a40cc2d1e9cbdd9276216a5ef
Benjawins <osuperpanda@gmail.com>
2022-04-26 12:18:40 +0200

    Create computerPlay to make computer's move
    function computerPlay() uses Math.floor(Math.random()*100) to
    generate an integer between 1-99 and we assign each third (33/99)
    to a respective value and return it example: 1-33 = 'Rock'.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,15 @@
 //Function to return 'Rock', 'Paper' or 'Scissors'
 
+function computerPlay() {
+    let outcome = Math.floor(Math.random()*100);
+
+    if (outcome <= 33) {
+        return 'Rock';
+    } 
+    else if (outcome > 33 && outcome <= 66) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+
+}
