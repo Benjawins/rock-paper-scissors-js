cc0808f759d0143f917d9f963ff034c5668b1cde
Benjawins <osuperpanda@gmail.com>
2022-04-26 13:33:05 +0200

    create playRound() that plays a round of the game
    Create function playRound() that plays a round of rock paper
    scissors and returns the result. Taking input from user through
    prompt and using computerPlay() function for computer's input.
    
    Note to self: Improvements for more efficient code exists.

--- scripts.js
+++ scripts.js
@@ -11,5 +11,34 @@ function computerPlay() {
     } else {
         return 'Scissors';
     }
-
 }
+
+//Function that plays single round of Rock Paper Scissors.
+function playRound(playerSelection, computerSelection) {
+   playerSelection = prompt("Rock, Paper or Scissors?", "").toLowerCase();
+   computerSelection = computerPlay().toLowerCase();
+
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return "You Lose! Paper beats Rock";
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return "You Lose! Scissors beats Paper";
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return "You Lose! Rock beats Scissors";
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return "You Win! Scissors beats Paper";
+    }  else {
+          return "Draw"
+      }
+}
+
+/* TESTING
+
+const playerSelection = "rock";
+const computerSelection = computerPlay().toLowerCase();
+console.log(playRound(playerSelection, computerSelection));
+
+*/ 
