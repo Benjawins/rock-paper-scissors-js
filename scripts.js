d42bdeca201771f13927641028b974e32d3b07fb
Benjawins <osuperpanda@gmail.com>
2022-04-26 18:01:08 +0200

    Add game() & fix score tracking w global variables
    Added game() function which calls playRound() 5 times keeps track
    of player and computer score through  global variables playerScore
    compScore. Using charAt method to return if the string was a win or
    loss.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
 //Function to return 'Rock', 'Paper' or 'Scissors'
 
+let playerScore = 0;
+let compScore = 0;
+
+
+console.log("*Welcome to the Rock Paper Scissors game, type 'game()' to get started*")
+
 function computerPlay() {
     let outcome = Math.floor(Math.random()*100);
 
@@ -15,30 +21,66 @@ function computerPlay() {
 
 //Function that plays single round of Rock Paper Scissors.
 function playRound(playerSelection, computerSelection) {
+
    playerSelection = prompt("Rock, Paper or Scissors?", "").toLowerCase();
    computerSelection = computerPlay().toLowerCase();
 
     if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    //    playerScore++;
         return "You Win! Rock beats Scissors";
     } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+    //    compScore++;
         return "You Lose! Paper beats Rock";
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+     //   playerScore++;
         return "You Win! Paper beats Rock";
     } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    //    compScore++;
         return "You Lose! Scissors beats Paper";
     } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+     //   compScore++;
         return "You Lose! Rock beats Scissors";
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+   //     playerScore++;
         return "You Win! Scissors beats Paper";
     }  else {
           return "Draw"
       }
+
 }
 
-/* TESTING
+function game() {
 
-const playerSelection = "rock";
-const computerSelection = computerPlay().toLowerCase();
-console.log(playRound(playerSelection, computerSelection));
 
-*/ 
+    for (let i = 0; i < 5; i++) {
+        let result = playRound();
+        console.log(result);
+ //       console.log(result.charAt(4));
+
+
+        if (result.charAt(4) == 'W') {
+            playerScore++;
+        } else if (result.charAt(4) == 'L') {
+            compScore++;
+        }
+
+ //       console.log(playerScore);
+
+        
+        if (i == 4 && playerScore > compScore) {
+            return 'Game ended ' +playerScore+ ' - ' +compScore+ ', You Win! Refresh page to play again'           
+        } else if (i == 4 && playerScore < compScore) {
+            return 'Game ended ' +playerScore+ ' - ' +compScore+ ', You Lose! Refresh page to play again'
+        } else if (i == 4 && playerScore == compScore) {
+            return 'Game ended ' +playerScore+ ' - ' +compScore+ ", It's a draw! Refresh page to play again"
+        }
+      //  return 'Game ended ' +a+ ' - ' +b+ '!';
+    /*     
+        if (a > b) {
+            return 'You won! Game ended ' +a+ ' - ' +b+'!';
+        }
+        */
+ 
+    }
+}  /*     if (playRound().charAt(4) = "L") {
+} */
